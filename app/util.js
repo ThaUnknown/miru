54993f1b070d0677ebcf55740704f47df9c288d5
Dennis Rönn <dennisronn@hotmail.com>
2020-09-15 21:19:38 +0200

    - Improved the code in util.js to not throw errors if halfmoon is removed in the future

--- app/util.js
+++ app/util.js
@@ -1,9 +1,11 @@
-halfmoon.showModal = id => {
-    const t = document.getElementById(id)
-    t && t.classList.add("show")
-}
-halfmoon.hideModal = id => {
-    const t = document.getElementById(id)
-    t && t.classList.remove("show")
+if ((typeof halfmoon === "object" || typeof halfmoon === 'function') && (halfmoon !== null)) {
+    halfmoon.showModal = id => {
+        const t = document.getElementById(id)
+        t && t.classList.add("show")
+    }
+    halfmoon.hideModal = id => {
+        const t = document.getElementById(id)
+        t && t.classList.remove("show")
+    }
 }
 
