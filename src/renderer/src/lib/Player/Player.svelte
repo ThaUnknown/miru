003231aa3939ab8b955688497432f1a25f3d45c1
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-09-19 17:40:51 +0200

    fix: close track controls on track change

--- src/renderer/src/lib/Player/Player.svelte
+++ src/renderer/src/lib/Player/Player.svelte
@@ -1002,48 +1002,47 @@
       <div class='ts mr-auto'>{toTS(targetTime, duration > 3600 ? 2 : 3)} / {toTS(duration - targetTime, duration > 3600 ? 2 : 3)}</div>
       {#if 'audioTracks' in HTMLVideoElement.prototype && video?.audioTracks?.length > 1}
         <div class='dropdown dropup with-arrow' on:click={toggleDropdown}>
-          <span class='material-icons ctrl' title='Audio Tracks' id='baudio' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false' data-name='audioButton'>
+          <span class='material-icons ctrl' title='Audio Tracks'>
             queue_music
           </span>
-          <div class='dropdown-menu dropdown-menu-left ctrl custom-radio p-10 pb-5 text-capitalize' aria-labelledby='baudio' data-name='selectAudio'>
+          <div class='dropdown-menu dropdown-menu-left ctrl custom-radio p-10 pb-5 text-capitalize'>
             {#each video.audioTracks as track}
               <input name='audio-radio-set' type='radio' id='audio-{track.id}-radio' value={track.id} checked={track.enabled} />
-              <label for='audio-{track.id}-radio' on:click={() => selectAudio(track.id)} class='text-truncate pb-5'>
-                {(track.language || (!Object.values(video.audioTracks).some(track => track.language === 'eng' || track.language === 'en') ? 'eng' : track.label)) +
-                  (track.label ? ' - ' + track.label : '')}</label>
+              <label for='audio-{track.id}-radio' on:click|stopPropagation={() => selectAudio(track.id)} class='text-truncate pb-5'>
+                {(track.language || (!Object.values(video.audioTracks).some(track => track.language === 'eng' || track.language === 'en') ? 'eng' : track.label)) + (track.label ? ' - ' + track.label : '')}
+              </label>
             {/each}
           </div>
         </div>
       {/if}
       {#if 'videoTracks' in HTMLVideoElement.prototype && video?.videoTracks?.length > 1}
         <div class='dropdown dropup with-arrow'>
-          <span class='material-icons ctrl' title='Video Tracks' id='bvideo' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false' data-name='videoButton'>
+          <span class='material-icons ctrl' title='Video Tracks'>
             playlist_play
           </span>
-          <div class='dropdown-menu dropdown-menu-left ctrl custom-radio p-10 pb-5 text-capitalize' aria-labelledby='bvideo' data-name='selectVideo'>
+          <div class='dropdown-menu dropdown-menu-left ctrl custom-radio p-10 pb-5 text-capitalize'>
             {#each video.videoTracks as track}
               <input name='video-radio-set' type='radio' id='video-{track.id}-radio' value={track.id} checked={track.selected} />
-              <label for='video-{track.id}-radio' on:click={() => selectVideo(track.id)} class='text-truncate pb-5'>
-                {(track.language || (!Object.values(video.videoTracks).some(track => track.language === 'eng' || track.language === 'en') ? 'eng' : track.label)) +
-                  (track.label ? ' - ' + track.label : '')}</label>
+              <label for='video-{track.id}-radio' on:click|stopPropagation={() => selectVideo(track.id)} class='text-truncate pb-5'>
+                {(track.language || (!Object.values(video.videoTracks).some(track => track.language === 'eng' || track.language === 'en') ? 'eng' : track.label)) + (track.label ? ' - ' + track.label : '')}
+              </label>
             {/each}
           </div>
         </div>
       {/if}
       {#if subHeaders?.length}
         <div class='subtitles dropdown dropup with-arrow' on:click={toggleDropdown}>
-          <span class='material-icons ctrl' title='Subtitles [C]' id='bcap' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false' data-name='captionsButton'>
+          <span class='material-icons ctrl' title='Subtitles [C]'>
             subtitles
           </span>
-          <div class='dropdown-menu dropdown-menu-right ctrl custom-radio p-10 pb-5 text-capitalize w-200' aria-labelledby='bcap' data-name='selectCaptions'>
+          <div class='dropdown-menu dropdown-menu-right ctrl custom-radio p-10 pb-5 text-capitalize w-200'>
             <input name='subtitle-radio-set' type='radio' id='subtitle-off-radio' value='off' checked={subHeaders && subs?.current === -1} />
-            <label for='subtitle-off-radio' on:click={() => subs.selectCaptions(-1)} class='text-truncate pb-5'> OFF </label>
+            <label for='subtitle-off-radio' on:click|stopPropagation={() => subs.selectCaptions(-1)} class='text-truncate pb-5'> OFF </label>
             {#each subHeaders as track}
               {#if track}
                 <input name='subtitle-radio-set' type='radio' id='subtitle-{track.number}-radio' value={track.numer} checked={track.number === subs.current} />
                 <label for='subtitle-{track.nubmer}-radio' on:click={() => subs.selectCaptions(track.number)} class='text-truncate pb-5'>
-                  {(track.language || (!Object.values(subs.headers).some(header => header.language === 'eng' || header.language === 'en') ? 'eng' : track.type)) +
-                    (track.name ? ' - ' + track.name : '')}
+                  {(track.language || (!Object.values(subs.headers).some(header => header.language === 'eng' || header.language === 'en') ? 'eng' : track.type)) + (track.name ? ' - ' + track.name : '')}
                 </label>
               {/if}
             {/each}
