2d3013e2261536602876ea62d0a2bad79d0939ad
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2023-05-16 21:59:04 +0200

    fix: mediahandler error

--- src/renderer/src/modules/util.js
+++ src/renderer/src/modules/util.js
@@ -12,17 +12,18 @@ export function countdown (s) {
   return tmp.join(' ')
 }
 
+const formatter = new Intl.RelativeTimeFormat('en')
+const ranges = {
+  years: 3600 * 24 * 365,
+  months: 3600 * 24 * 30,
+  weeks: 3600 * 24 * 7,
+  days: 3600 * 24,
+  hours: 3600,
+  minutes: 60,
+  seconds: 1
+}
+
 export function since (date) {
-  const formatter = new Intl.RelativeTimeFormat('en')
-  const ranges = {
-    years: 3600 * 24 * 365,
-    months: 3600 * 24 * 30,
-    weeks: 3600 * 24 * 7,
-    days: 3600 * 24,
-    hours: 3600,
-    minutes: 60,
-    seconds: 1
-  }
   const secondsElapsed = (date.getTime() - Date.now()) / 1000
   for (const key in ranges) {
     if (ranges[key] < Math.abs(secondsElapsed)) {
