517b9ce3e8e9e13fdfca7f9e4b16e819af80e104
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2023-07-12 23:37:52 +0200

    fix: #295

--- src/renderer/views/Player/MediaHandler.svelte
+++ src/renderer/views/Player/MediaHandler.svelte
@@ -68,6 +68,8 @@
     }
   }
 
+  const TYPE_EXCLUSIONS = ['ED', 'ENDING', 'NCED', 'NCOP', 'OP', 'OPENING', 'PREVIEW', 'PV']
+
   async function handleFiles (files) {
     console.info('MediaHandler: got files', files)
     if (!files?.length) return processed.set(files)
@@ -87,11 +89,7 @@
       return file
     })
 
-    videoFiles = videoFiles.filter(file => {
-      if (file.media.parseObject.anime_type?.toLowerCase() === 'nced') return false
-      if (file.media.parseObject.anime_type?.toLowerCase() === 'ncop') return false
-      return true
-    })
+    videoFiles = videoFiles.filter(file => !TYPE_EXCLUSIONS.includes(file.media.parseObject.anime_type))
 
     console.info('MediaHandler: resolved video files', { videoFiles })
 
