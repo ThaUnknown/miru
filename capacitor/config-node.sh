18ef3153dbbe39d24dcabda6617ff164d86290eb
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2023-12-06 10:11:07 +0100

    feat(capacitor): nodejs mobile native dependencies

--- capacitor/config-node.sh
+++ capacitor/config-node.sh
@@ -3,9 +3,6 @@
 # instructions
 # Install Android NDK
 # Set $ANDROID_NDK_PATH (example: ~/Android/Sdk/ndk/26.1.10909125)
-# Download and extract android .zip for nodejs-mobile from https://github.com/nodejs-mobile/nodejs-mobile/releases/tag/v18.17.2
-# Update LIBNODE_PATH
-# npm install nodejs-mobile-gyp
 # install other npm packages like normal
 # run this script
 
@@ -13,7 +10,7 @@ toolchain_target_arch=aarch64
 node_target_arch=arm64
 android_api_level=22
 toolchain_folder=$ANDROID_NDK_PATH/toolchains/llvm/prebuilt/linux-x86_64/bin
-export LIBNODE_PATH=~/Downloads/nodejs-mobile
+export LIBNODE_PATH=$(pwd)/git_modules/nodejs-mobile
 
 export PATH=$toolchain_folder:$PATH
 
@@ -24,7 +21,7 @@ export AR=$toolchain_folder/llvm-ar
 
 export npm_config_verbose=1
 export npm_config_nodedir=${LIBNODE_PATH}
-export npm_config_node_gyp=$(pwd)/node_modules/nodejs-mobile-gyp/bin/node-gyp.js
+export npm_config_node_gyp=$(pwd)/../node_modules/nodejs-mobile-gyp/bin/node-gyp.js
 export npm_config_arch=${node_target_arch}
 export npm_config_plaform=android
 export npm_config_format=make-android
@@ -32,7 +29,7 @@ export npm_gyp_defines="target_arch=$node_target_arch v8_target_arch=$node_targe
 
 #mv node_modules ../node_modules.bak
 # --from-from-source is used by node-pre-gyp
-npm rebuild --build-from-source
+cd resources/nodejs && npm rebuild --build-from-source
 # Remove executable permissions from native node modules
 # find node_modules -iname '*.node' -exec chmod -x '{}' \;
 
