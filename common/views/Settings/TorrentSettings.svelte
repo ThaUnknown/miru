e91f65b4d13011150e68206d720ba8725c5030a7
RockinChaos <JacobGaming@CraftationGaming.com>
2024-08-01 16:24:30 -0700

    chore: cleanup

--- common/views/Settings/TorrentSettings.svelte
+++ common/views/Settings/TorrentSettings.svelte
@@ -142,8 +142,7 @@
 <h4 class='mb-10 font-weight-bold'>Client Settings</h4>
 {#if SUPPORTS.torrentPath}
   <SettingCard title='Torrent Download Location' description='Path to the folder used to store torrents. By default this is the TMP folder, which might lose data when your OS tries to reclaim storage.'>
-    <div
-      class='input-group w-300 mw-full'>
+    <div class='input-group w-300 mw-full'>
       <div class='input-group-prepend'>
         <button type='button' use:click={handleFolder} class='btn btn-primary input-group-append'>Select Folder</button>
       </div>
