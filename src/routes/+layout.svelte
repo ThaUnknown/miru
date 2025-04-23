bbc58c4e0c8e5f184a5236e8ebf417b55372b231
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2025-04-23 19:35:07 +0200

    chore: LOL

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,11 +8,18 @@
   import { Menubar } from '$lib/components/ui/menubar'
   import { Toaster } from '$lib/components/ui/sonner'
   import Online from '$lib/components/Online.svelte'
+  import native from '$lib/modules/native'
 
   let root: HTMLDivElement
+
+  let updateProgress = 0
+
+  native.updateProgress(progress => {
+    updateProgress = progress
+  })
 </script>
 
-<div class='w-full h-full backface-hidden bg-black relative overflow-clip border-l-2 border-white preserve-3d' bind:this={root} id='root'>
+<div class='w-full h-full backface-hidden bg-black relative overflow-clip border-l-2 [border-image:linear-gradient(to_bottom,white_var(--progress),#2dcf58_var(--progress))_1] preserve-3d' bind:this={root} id='root' style:--progress='{100 - updateProgress}%'>
   <ProgressBar zIndex={100} />
   <Toaster />
 
